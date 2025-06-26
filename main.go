de348a139d224398e2a7e3315d54a4ab472aefb5
曲维新 <quweixin@baoqi56.com>
2025-06-26 10:51:05 +0800

    完成回文任务

--- main.go
+++ main.go
@@ -6,7 +6,18 @@ import (
 )
 
 func main() {
+	// task01_出现一次的数字
 	var numbs = []int{4, 1, 2, 1, 2}
 	result := task01.SingleNumber(numbs)
 	fmt.Println(result)
+	// task01_回文
+	//var s = "A man, a plan, a canal: Panama"
+	//var s = "madam"
+	var s = "上海自来水来自海上"
+	var isPalindrome = task01.IsPalindrome(s)
+	if isPalindrome {
+		fmt.Println("是回文")
+	} else {
+		fmt.Println("不是回文")
+	}
 }
---
+++ task01/task01_回文.go
@@ -0,0 +1,13 @@
+package task01
+
+/**
+ * 判断字符串是否是回文
+ */
+func IsPalindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
