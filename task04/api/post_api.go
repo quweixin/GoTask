16882cae4512c9751eb854d631c1ca57c7a259dc
曲维新 <quweixin@baoqi56.com>
2025-07-11 09:14:10 +0800

    完成post 修改文章 接口

--- task04/api/post_api.go
+++ task04/api/post_api.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"gorm.io/gorm"
 	"net/http"
 )
 
@@ -40,7 +41,40 @@ func CreatePost(context *gin.Context) {
 }
 
 func UpdatePost(context *gin.Context) {
-
+	var postForm forms.PostForm
+	if err := context.ShouldBind(&postForm); err != nil {
+		var errs validator.ValidationErrors
+		ok := errors.As(err, &errs)
+		if !ok {
+			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		context.JSON(http.StatusOK, gin.H{
+			"code":    http.StatusBadRequest,
+			"message": utils.ErrorFormat(errs.Translate(global.Trans)),
+		})
+	}
+	//id := context.Param("id")
+	idStr := context.Param("id")
+	id := utils.ParamToUint(idStr)
+	var post model.Post
+	global.DB.First(&post, id)
+	if post.ID == 0 {
+		context.JSON(http.StatusOK, gin.H{
+			"code":    http.StatusBadRequest,
+			"message": "文章不存在",
+		})
+	}
+	post = model.Post{
+		Title:   utils.ToNullString(postForm.Title),
+		Content: utils.ToNullString(postForm.Content),
+	}
+	//global.DB.Model(&post).Where("id = ? and user_id=?", id, context.MustGet("userId").(uint)).Updates(&post)
+	global.DB.Model(&post).Where(&model.Post{Model: gorm.Model{ID: id}, UserId: context.MustGet("userId").(uint)}).Updates(&post)
+	context.JSON(http.StatusOK, gin.H{
+		"code":    http.StatusOK,
+		"message": "更新文章成功",
+	})
 }
 
 func DeletePost(context *gin.Context) {
--- task04/utils/type_convertor.go
+++ task04/utils/type_convertor.go
@@ -1,10 +1,26 @@
 package utils
 
-import "database/sql"
+import (
+	"database/sql"
+	"strconv"
+	"strings"
+)
 
 func ToNullString(s string) sql.NullString {
 	return sql.NullString{
 		String: s,
 		Valid:  s != "",
 	}
+}
+
+func ParamToUint(s string) uint {
+	s = strings.TrimSpace(s)
+	n64, err := strconv.ParseUint(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	if n64 == 0 {
+		return 0
+	}
+	return uint(n64)
 }
